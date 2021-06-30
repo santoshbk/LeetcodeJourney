fdf9dd560f7ccc300a067c1c6df68258616aa56d
Santosh K <bornfree.kushi@gmail.com>
2021-06-30 16:33:27 +0530

    new problem

---
+++ src/com/santosh/array/Consecutive1s.java
@@ -0,0 +1,12 @@
+package com.santosh.array;
+
+public class Consecutive1s {
+
+    public static void main(String[] args) {
+
+    }
+    public int findMaxConsecutiveOnes(int[] nums) {
+
+        return 0;
+    }
+}
